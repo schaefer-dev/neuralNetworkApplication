e67be112d79fd42cdb7ab09ee179162a60d12c30
Dominik Weber <weberdom@web.de>
2016-11-21 13:12:13 +0100

    2.3a und 2.4a

--- abgaben/abgabe02/gradient_descent.py
+++ abgaben/abgabe02/gradient_descent.py
@@ -4,18 +4,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-def f( x , y ):
-	return 20*x*x + y*y/4
+def f( x ):
+	return 20*x[0,0]*x[0,0] + x[1,0]*x[1,0]/4
 
 
-def fpx( x , y ):
-	return 40*x
-
-def fpy( x , y ):
-	return y/2
+def fpx( x ):
+	return matrix([[20*x[0,0]],[x[1,0]/2]])
 # Initial point
-x=-2
-y=4
+x= matrix([[-2],[4]])
 
 epsilon=0.04
 
@@ -32,14 +28,8 @@ epsilon=0.04
 #fig.colorbar(surf, shrink=0.5, aspect=5)
 #colors = ('r', 'g', 'b', 'k')
 
-for i in range(0, 100):
-	#print x, y, f(x,y)
-	x1 = x - epsilon * fpx(x,y)
-	y1 = y - epsilon * fpy(x,y)
-	x = x1
-	y = y1
-	#ax.scatter(x, y, f(x,y), zdir='z', c='yellow')
-
-print int(x), int(y)
-
+for i in range(0, 1000):
+	print x,f(x)
+	x = x - epsilon * fpx(x)
+	#ax.scatter(x, f(x), zdir='z', c='yellow')
 #plt.show()
