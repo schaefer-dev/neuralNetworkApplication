1cef49392b6c7c044c09088f37535cdaad79cfb3
Daniel Schäfer <schaefer.dev@gmail.com>
2016-11-22 05:51:12 +0100

    refactoring code according to PEP8

--- abgaben/abgabe02/gradient_descent.py
+++ abgaben/abgabe02/gradient_descent.py
@@ -4,32 +4,33 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-def f( x ):
-	return 20*x[0,0]*x[0,0] + x[1,0]*x[1,0]/4
+
+def f(x):
+    return 20 * x[0, 0] * x[0, 0] + x[1, 0] * x[1, 0] / 4
 
 
-def fpx( x ):
-	return matrix([[20*x[0,0]],[x[1,0]/2]])
+def fpx(x):
+    return matrix([[20 * x[0, 0]], [x[1, 0] / 2]])
 # Initial point
-x= matrix([[-2],[4]])
+x = matrix([[-2], [4]])
 
-epsilon=0.04
+epsilon = 0.04
 
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
-#X = np.arange(-5, 5, 0.25)
-#Y = np.arange(-5, 5, 0.25)
-#X, Y = np.meshgrid(X, Y)
-#Z = f(X,Y)
-#surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-#ax.set_zlim(-1, 600)
-#ax.zaxis.set_major_locator(LinearLocator(10))
-#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-#fig.colorbar(surf, shrink=0.5, aspect=5)
-#colors = ('r', 'g', 'b', 'k')
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# X = np.arange(-5, 5, 0.25)
+# Y = np.arange(-5, 5, 0.25)
+# X, Y = np.meshgrid(X, Y)
+# Z = f(X,Y)
+# surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+# ax.set_zlim(-1, 600)
+# ax.zaxis.set_major_locator(LinearLocator(10))
+# ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+# colors = ('r', 'g', 'b', 'k')
 
 for i in range(0, 1000):
-	print x,f(x)
-	x = x - epsilon * fpx(x)
-	#ax.scatter(x, f(x), zdir='z', c='yellow')
-#plt.show()
+    print(x, f(x))
+    x = x - epsilon * fpx(x)
+    # ax.scatter(x, f(x), zdir='z', c='yellow')
+# plt.show()
--- abgaben/abgabe02/newton_method.py
+++ abgaben/abgabe02/newton_method.py
@@ -2,25 +2,26 @@ from math import pow
 import numpy as np
 from pylab import *
 
-def f( x ):
-	return 20*x[0,0]*x[0,0] + x[1,0]*x[1,0]/4
 
-def fp( x ):
-	return matrix([[20*x[0,0]],[x[1,0]/2]])
+def f(x):
+    return 20 * x[0, 0] * x[0, 0] + x[1, 0] * x[1, 0] / 4
 
-def hfi( x ):
-	 H = matrix([[40,0],[0,0.5]])
-	 Hi = H.I
-	 return Hi
+
+def fp(x):
+    return matrix([[20 * x[0, 0]], [x[1, 0] / 2]])
+
+
+def hfi(x):
+    H = matrix([[40, 0], [0, 0.5]])
+    Hi = H.I
+    return Hi
 
 # Initial point
-x= matrix([[-2],[4]])
+x = matrix([[-2], [4]])
 
-#Learning rate
-epsilon=0.5
+# Learning rate
+epsilon = 0.5
 
 for i in range(0, 30):
-	print x,f(x)
-	x = x - epsilon * hfi(x)*fp(x)
-
-
+    print(x, f(x))
+    x = x - epsilon * hfi(x) * fp(x)
