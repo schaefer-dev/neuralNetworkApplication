c4dde2c5aff51c1115e46e196b9cc333c2758cc1
Daniel Schäfer <schaefer.dev@gmail.com>
2016-11-22 16:00:12 +0100

    improved plotting + refactor

--- abgaben/abgabe02/source/gradient_descent.py
+++ abgaben/abgabe02/source/gradient_descent.py
@@ -4,9 +4,6 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-def fun(x,y):
-    return 20 * x * x + 1/4 * y * y
-
 
 def f(x):
     return 20 * x[0, 0] * x[0, 0] + x[1, 0] * x[1, 0] / 4
@@ -37,11 +34,12 @@ epsilon = 0.04
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-xh = yh = np.arange(-6.0, 6.0, 0.05)
+xh = yh = np.arange(-10.0, 10.0, 0.05)
 Xh, Yh = np.meshgrid(xh, yh)
-zs = np.array([fun(xh,yh) for xh,yh in zip(np.ravel(Xh), np.ravel(Yh))])
+zs = np.array([f(matrix([[xh],[yh]])) for xh,yh in zip(np.ravel(Xh), np.ravel(Yh))])
 Zh = zs.reshape(Xh.shape)
 
+# Drawing of the function surface
 ax.plot_surface(Xh, Yh, Zh)
 
 ax.set_xlabel('X Label')
@@ -57,7 +55,10 @@ for i in range(0, 1000):
     coordX = x.item(0)
     coordY = x.item(1)
     fxy = f(x)
+
+    # Drawing of the gradient descent
     ax.scatter(coordX,coordY,fxy, c='yellow')
+
     print("X = " + str(coordX))
     print("Y = " + str(coordY))
     print ("f(x,y) = " + str(fxy))
