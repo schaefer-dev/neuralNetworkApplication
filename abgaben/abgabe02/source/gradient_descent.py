7e08c93856c9db5d87fcc2725eb3eaac04bb3cec
Daniel Schäfer <schaefer.dev@gmail.com>
2016-11-22 15:27:11 +0100

    plotting alternative

--- abgaben/abgabe02/source/gradient_descent.py
+++ abgaben/abgabe02/source/gradient_descent.py
@@ -4,6 +4,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
+def fun(x,y):
+    return 20 * x * x + 1/4 * y * y
+
 
 def f(x):
     return 20 * x[0, 0] * x[0, 0] + x[1, 0] * x[1, 0] / 4
@@ -29,8 +32,34 @@ epsilon = 0.04
 # fig.colorbar(surf, shrink=0.5, aspect=5)
 # colors = ('r', 'g', 'b', 'k')
 
+
+## alternative plotting Begin ---------------
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+xh = yh = np.arange(-6.0, 6.0, 0.05)
+Xh, Yh = np.meshgrid(xh, yh)
+zs = np.array([fun(xh,yh) for xh,yh in zip(np.ravel(Xh), np.ravel(Yh))])
+Zh = zs.reshape(Xh.shape)
+
+ax.plot_surface(Xh, Yh, Zh)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+
+## alternative plotting End ---------------
+
+
+
 for i in range(0, 1000):
-    print(x, f(x))
+    print("X = " + str(x.item(0)))
+    print("Y = " + str(x.item(1)))
+    print ("f(x,y) = " + str(f(x)))
+    print("---------------------------")
+    # print(x, f(x))
     x = x - epsilon * fpx(x)
     # ax.scatter(x, f(x), zdir='z', c='yellow')
-# plt.show()
+
+plt.show()
