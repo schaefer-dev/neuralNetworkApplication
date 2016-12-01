7c6e66b70f98c61d89b0dbdba74d64c9ccbc730d
Daniel Schäfer <schaefer.dev@gmail.com>
2016-12-01 20:59:50 +0100

    array plotting finished

--- abgaben/abgabe04/source/kmeans.py
+++ abgaben/abgabe04/source/kmeans.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+
+# Parameters
+plotting = True
 
 # Array used to store data
 array = []
@@ -10,4 +14,7 @@ with open('data_kmeans.txt') as f:
 
 
 # Plotting of array
-
+if (plotting):
+    for i in range(0,len(array)):
+        plt.plot(array[i][0],array[i][1], 'ro')
+    plt.show()
