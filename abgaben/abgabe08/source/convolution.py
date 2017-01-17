97a2a9d1e862e53162407b76aa809303b75fa6ae
Daniel Schäfer <schaefer.dev@gmail.com>
2017-01-17 16:24:19 +0100

    first version of fixed convolution

--- abgaben/abgabe08/source/convolution.py
+++ abgaben/abgabe08/source/convolution.py
@@ -2,8 +2,40 @@ from scipy import misc
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def readImage(path):
     return misc.imread(path)
+
+
+def convfix(image,kernel):
+
+   kernel_width = kernel.shape[0]
+   kernel_height = kernel.shape[1]
+   image_width = image.shape[0]
+   image_height = image.shape[1]
+
+   width_shift = kernel_width // 2
+   height_shift = kernel_height // 2
+
+   padded_image = np.zeros((image_width + width_shift, image_height + height_shift))
+
+   # image padded with as many zeros as necessary
+   for x in range (0,image_width-1):
+      for y in range(0,image_height-1):
+         padded_image[x + width_shift, y + height_shift] = image[x,y]
+
+   conv_image = np.zeros((image_width,image_height))
+
+   for x in range(0,(image_width - 1)):
+       for y in range(0,(image_height - 1)):
+           for a in range(-width_shift, width_shift):
+               for b in range(-height_shift, height_shift):
+                   conv_image[x,y] += padded_image[x + width_shift + a, y + height_shift + b] * kernel[a + width_shift ,b + height_shift]
+
+
+   return conv_image
+
+
 
 def conv(image, kernel):
     # ex 8.2 a)
@@ -44,6 +76,7 @@ def min_max_rescale(image):
             res[x][y] = (image[x][y] - minV) / float(rangeV) * 255
 
     return res
+
 
 def main():
    # ex 8.2 b)
