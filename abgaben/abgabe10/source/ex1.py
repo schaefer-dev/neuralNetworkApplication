df1ac7c247f11082be04da08aecc4b39977d74e8
Daniel Schäfer <schaefer.dev@gmail.com>
2017-01-31 19:01:42 +0100

    implemented plotting for 1c

--- abgaben/abgabe10/source/ex1.py
+++ abgaben/abgabe10/source/ex1.py
@@ -1,19 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def Power(M):
     u = np.random.random((M.shape[0],1))
     v = np.zeros((M.shape[0],1))
-    end=0;
+    end = 0.0;
     i = 0
-    while(end!=1):
+    plots = []
+    while(end != 1.0):
         v = u
         Mu = M*u
         mnM = np.linalg.norm(Mu)
         u = Mu/mnM
         u = u/np.linalg.norm(u)
-        end = np.abs(int(np.matmul(v.T,u)))
-        print(end)
-    return (u)
 
+        # print i-th iteration
+
+
+        # end = np.abs(int(np.matmul(v.T,u)))
+        end = np.linalg.norm(np.matmul(v.T,u))
+        plt.scatter(i,end)
+
+        print(end)
+        i += 1
+    return (u)
 M = np.matrix([[-2,-2,3],[-10,-1,6],[10,-2,-9]])
 print(Power(M))
+
+plt.show()
