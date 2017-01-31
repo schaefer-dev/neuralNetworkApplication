956c96f87fa161183a11f4fb2409cb3bff72fbe9
Dominik Weber <weberdom@web.de>
2017-01-31 18:42:44 +0100

    ex1

--- abgaben/abgabe09/source/ex1.py
+++ abgaben/abgabe09/source/ex1.py
@@ -2,15 +2,17 @@ import numpy as np
 
 def Power(M):
     u = np.random.random((M.shape[0],1))
-    u1 = np.zeros((M.shape[0],1))
+    v = np.zeros((M.shape[0],1))
     end=0;
     i = 0
-    while(np.array_equal(u,u1)!=True):
-        u1 = u
+    while(end!=1):
+        v = u
         Mu = M*u
         mnM = np.linalg.norm(Mu)
         u = Mu/mnM
         u = u/np.linalg.norm(u)
+        end = np.abs(int(np.matmul(v.T,u)))
+        print(end)
     return (u)
 
 M = np.matrix([[-2,-2,3],[-10,-1,6],[10,-2,-9]])
