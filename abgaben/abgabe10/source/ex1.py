03e9843822cb3ffe670e31822b8850563a019392
Daniel Schäfer <schaefer.dev@gmail.com>
2017-01-31 19:07:37 +0100

    minor improvments

--- abgaben/abgabe10/source/ex1.py
+++ abgaben/abgabe10/source/ex1.py
@@ -6,7 +6,7 @@ def Power(M):
     v = np.zeros((M.shape[0],1))
     end = 0.0;
     i = 0
-    plots = []
+
     while(end != 1.0):
         v = u
         Mu = M*u
@@ -14,17 +14,15 @@ def Power(M):
         u = Mu/mnM
         u = u/np.linalg.norm(u)
 
-        # print i-th iteration
-
-
-        # end = np.abs(int(np.matmul(v.T,u)))
         end = np.linalg.norm(np.matmul(v.T,u))
-        plt.scatter(i,end)
 
-        print(end)
+        # print i-th iteration
+        plt.scatter(i,end)
+        print("Itaration " + str(i) + " results in value: " + str(end))
+
         i += 1
     return (u)
 M = np.matrix([[-2,-2,3],[-10,-1,6],[10,-2,-9]])
-print(Power(M))
+print("\nResulting Eigenvector:\n" + str(Power(M)))
 
 plt.show()
